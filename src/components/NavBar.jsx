51494950bb8b7509f236fc00a37c4d312e51142f
isaiasd2712 <isaiasd2712@gmail.com>
2024-01-09 12:26:21 +0100

    NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,31 @@
-export default function NavBar(){
-    return(
-    <div>
-        <p>hola</p>
-    </div>
-    )
+import React, { useState } from 'react';
+import './NavBar.css';
+import { NavLink } from 'react-router-dom';
+
+export default function NavBar() {
+    const [isMenuVisible, setMenuVisible] = useState(false);
+
+    const handleMenuToggle = () => {
+        setMenuVisible(!isMenuVisible);
+    };
+
+    const handleNavLinkClick = () => {
+        // Close the menu when clicking on a link
+        setMenuVisible(false);
+    };
+
+    return (
+        <div className='navBar'>
+            <nav className={`menu ${isMenuVisible ? 'show' : 'hide'}`}>
+                <ul className='menuList'>
+                    <li className='route'><NavLink to="/" className='textRoute' activeClassName='active' onClick={handleNavLinkClick}>HOME</NavLink></li>
+                    <li className='route'><NavLink to="/biography" className='textRoute' activeClassName='active' onClick={handleNavLinkClick}>BIOGRAPHY</NavLink></li>
+                    <li className='route'><NavLink to="/events"  className='textRoute' activeClassName='active' onClick={handleNavLinkClick}>EVENTS</NavLink></li>
+                    <li className='route'><NavLink to="/music" className='textRoute' activeClassName='active' onClick={handleNavLinkClick}>MUSIC</NavLink></li>
+                    <li className='route'><NavLink to="/video" className='textRoute' activeClassName='active' onClick={handleNavLinkClick}>VIDEO</NavLink></li>
+                    <li className='route'><NavLink to="/contact" className='textRoute' activeClassName='active' onClick={handleNavLinkClick}>CONTACT</NavLink></li>
+                </ul>
+            </nav>
+        </div>
+    );
 }
