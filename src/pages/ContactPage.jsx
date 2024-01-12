c43196a39efc96e48428d04dbc3d03ae88559209
isaiasd2712 <isaiasd2712@gmail.com>
2024-01-12 21:05:23 +0100

    Link Created

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -3,10 +3,19 @@ import './ContactPage.css'
 
 function ContactPage() {
   return (
-    <section className=''>
-    <div>ContactPage</div>
+    <section className='contact'>
+      <div className='blurContact'>
+      <div className='titleContainer'>
+        <h2 className='title'>CONTACT ME</h2>
+      </div>
+      <div className='paragraphContainer'>
+        <p className='paragraph'>Booking: <br/>
+          <a className='emailLink' href='https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSHvnntPFjPwPpRjWsjTrXgpdmLwpQNVJdVrtNSjpXJPRWnqhGKRWqTPSPPnbzMszjkdCSGb' target='_blank'>rich@midnightmango.co.uk</a>
+        </p>
+      </div>
+      </div>
     </section>
   )
 }
 
-export default ContactPage
+export default ContactPage;
