ecd1ef96bfbaf380f4dcf1112bc9ed8eb5f96e20
isaiasd2712 <isaiasd2712@gmail.com>
2024-01-17 23:52:35 +0100

    efectos chulos

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -22,14 +22,26 @@ function ContactPage() {
     return () => clearInterval(interval);
   }, []); // La dependencia vacía asegura que el efecto se ejecute solo una vez al montar el componente
 
+  const handleAnimationStart = () => {
+    const formContainer = document.querySelector('.blurContact');
+    formContainer.classList.add('appear');
+  };
+
   return (
     <section className='contact'>
-      <div className='blurContact' style={{ backdropFilter: `blur(${blurValue}px)` }}>
-        {/* Contenido de tu página */}
+      <div className={`blurContact`} style={{ backdropFilter: `blur(${blurValue}px)` }} onAnimationStart={handleAnimationStart}>
+        <form action="" className='contactForm'>
+          <input type="text" name='name' placeholder='Your Name' className='contactInputs' required/>
+          <input type="email" name='email' placeholder='Your Email' className='contactInputs' required/>
+          <textarea name="message" placeholder='Your Message' className='contactImputs' required></textarea>
+          <button type='submit'>Submit</button>
+        </form>
       </div>
     </section>
   );
 }
 
 export default ContactPage;
+
+
 
