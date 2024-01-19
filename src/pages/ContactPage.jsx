47a4aa679f66644e3acb033363603ae8488ebee2
isaiasd2712 <isaiasd2712@gmail.com>
2024-01-19 09:59:23 +0100

    code smelth

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -8,22 +8,21 @@ function ContactPage() {
   let interval;
 
   useEffect(() => {
-    // Simulación de carga de página
     interval = setInterval(() => {
       setBlurValue((prevBlur) => {
         if (prevBlur < 3) {
           return prevBlur + 1;
         } else {
           clearInterval(interval);
-          setPageLoaded(true); // Marcar la página como cargada al finalizar la simulación
+          setPageLoaded(true); 
           return prevBlur;
         }
       });
     }, 100);
 
-    // Limpiar el intervalo al desmontar el componente
+    
     return () => clearInterval(interval);
-  }, []); // La dependencia vacía asegura que el efecto se ejecute solo una vez al montar el componente
+  }, []); 
 
   return (
     <section className='contact'>
