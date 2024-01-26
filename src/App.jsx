02bf22e2f1c3ce8223fe5c3486ae82b8b2b5546c
limematheuz <mat.rib.lima@gmail.com>
2024-01-26 22:29:39 +0100

    music/video components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
-import Router  from "../src/Router";
+import Router from "../src/Router";
 import NavBar from "./components/NavBar";
 import "./App.css";
 
 function App() {
-
   return (
     <div className="appContainer">
-      <NavBar/>
+      <NavBar />
       <Router />
-      </div>
-      
-    
-  )
+    </div>
+  );
 }
 
 export default App;
-
