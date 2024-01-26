060fb1e779b47de13d38a4caa56c9abb6bc05356
isaiasd2712 <isaiasd2712@gmail.com>
2024-01-26 23:01:21 +0100

    footer in all pages

--- src/pages/MusicPage.jsx
+++ src/pages/MusicPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Music from "../components/music/Music";
+import Footer from "../components/Footer";
 
 export default function MusicPage() {
   return (
   <div>
     <Music/>
+    <Footer/>
   </div>
   );
 }
--- src/pages/VideoPage.jsx
+++ src/pages/VideoPage.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import Video from "../components/video/Video" 
+import Footer from "../components/Footer";
 
 export default function VideoPage(){
     return(
     <div>
         <Video/>
+        <Footer/>
     </div>
     )
 }
