bab8624632172c3470d4c548d59c0a302a6439f5
limematheuz <mat.rib.lima@gmail.com>
2024-01-29 12:03:36 +0100

    music

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,8 +1,9 @@
 import "./Gallery.css";
 import React from "react";
 import bannergallery from "../../src/assets/covers/nasiriHome.jpg"
+import arrow315dg from "../../src/assets/icons/arrow-right-solid-315dg.svg"
 
-const Gallery = ({ title, images, imageLinks, btn, captions }) => {
+const Gallery = ({ title, images, imageLinks, btn,}) => {
   return (
     <section className="galleryContainer">
       <div className="bannerGallery">
@@ -12,10 +13,11 @@ const Gallery = ({ title, images, imageLinks, btn, captions }) => {
       <div className="imgGalleryContainer">
         {images && images.map((image, index) => (
           <div key={index} className="galleryItemContainer">
-            <a href={imageLinks[index]} target="_blank" rel="noopener noreferrer">
+            <a href={imageLinks[index]} target="_blank" rel="noopener noreferrer"/>
                 <img className="galleryImg" src={image} alt={`Slide ${index}`} />
-                <button className="galleryBtn">{btn}</button>
-            </a>
+                <button className="galleryBtn">{btn} 
+                  <img className="diagonalArrow" src={arrow315dg} alt="" />
+                </button>
           </div>
         ))}
       </div>
